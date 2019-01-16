564ad429b8870cbad3b22c7e5e5e4d20a965d3a4
Vladimír Macháček <8machy@seznam.cz>
2019-01-16 20:24:47 +0100

    Added properties descriptor

---
+++ src/render/binders/bindPropertyDescriptiors.js
@@ -0,0 +1,28 @@
+import {cloneObject, each} from "../../shared/utils";
+import {redrawInstance} from "../redrawler";
+import {renderingInstancesStatuses} from "../runtime/renderingInstances";
+
+
+/**
+ * @param {{}} renderingInstance
+ */
+export function bindPropertyDescriptors(renderingInstance) {
+	renderingInstance._data = renderingInstance._data ? cloneObject(renderingInstance.data) : {};
+
+	each(renderingInstance.data, function (propertyKey, propertyValue) {
+			renderingInstance._data[propertyKey] = propertyValue;
+
+			Object.defineProperty(renderingInstance.data, propertyKey, {
+				get: function () {
+					return renderingInstance._data[propertyKey];
+				},
+				set: function (value) {
+					renderingInstance._data[propertyKey] = value;
+
+					if (renderingInstance._status === renderingInstancesStatuses.rendered) {
+						redrawInstance(renderingInstance.id);
+					}
+				}
+			});
+	});
+}
