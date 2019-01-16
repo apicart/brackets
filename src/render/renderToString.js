0c94e18fca053fffeaaad35b17c7ca168a7d812a
Vladimír Macháček <8machy@seznam.cz>
2019-01-16 20:26:50 +0100

    Change rendering instances status during the process

--- src/render/renderToString.js
+++ src/render/renderToString.js
@@ -1,33 +1,40 @@
-import {components, filters, templateLiteralsEnabled} from '../shared/variables';
-import {selectorAttributeName} from './shared/variables';
-import {cloneObject, each} from '../shared/utils';
-import {tokenizeTemplate} from '../compiler/tokenizeTemplate';
-import {compileTemplate} from '../compiler/compileTemplate';
+import {templateLiteralsEnabled, selectorAttributeName, eventHandlersAttributeName} from '../shared/variables';
+import {each} from '../shared/utils';
+import {tokenizeTemplate} from './compiler/tokenizeTemplate';
+import {compileTemplate} from './compiler/compileTemplate';
+import {getFilter} from "./runtime/filters";
+import {getComponents} from "./runtime/components";
+import {renderingInstancesStatuses} from "./runtime/renderingInstances";
+
 
 var templatesCache = {};
 
+
 /**
- * @param {{}} parameters
- * @return {*}
+ * @param {{}} renderingInstance
+ * @return {{}}
  */
-export function renderToString(parameters) {
-	if (typeof parameters.beforeRender === 'function') {
-		parameters.beforeRender.call(parameters);
+export function renderToString(renderingInstance) {
+	renderingInstance._setStatus(renderingInstancesStatuses.processing);
+
+	if (typeof renderingInstance.beforeRender === 'function') {
+		renderingInstance.beforeRender.call(renderingInstance);
 	}
 
 	var
-		cacheKey = parameters.cacheKey || null,
+		cacheKey = renderingInstance.cacheKey,
 		cacheKeyIsSet = typeof cacheKey === 'string',
 		compiledTemplate,
-		data = parameters.data ? cloneObject(parameters.data) : {},
-		template = parameters.template || null,
-		templateArguments = [];
-
-	data['_runtime'] = {
-		components: components,
-		filters: filters,
-		renderedComponents: []
-	};
+		data = renderingInstance.data,
+		runtime = {
+			parentInstance: renderingInstance.id,
+			components: getComponents(),
+			getFilter: getFilter,
+			renderedComponents: []
+		},
+		template = renderingInstance.template,
+		templateArguments = [runtime],
+		templateParametersNames = ['_runtime'];
 
 	if (cacheKeyIsSet && cacheKey in templatesCache) {
 		compiledTemplate = templatesCache[cacheKey];
@@ -39,33 +46,37 @@ export function renderToString(parameters) {
 		}
 
 		var tokens = tokenizeTemplate(template);
-		compiledTemplate = compileTemplate(tokens, data);
+		compiledTemplate = compileTemplate(tokens, templateParametersNames.concat(Object.keys(data)));
 
 		if (cacheKeyIsSet && ! (cacheKey in templatesCache)) {
 			templatesCache[cacheKey] = compiledTemplate;
 		}
 	}
 
-	if (data) {
-		each(data, function (key, value) {
-			templateArguments.push(value);
-		});
-	}
+	each(data, function (key, value) {
+		templateArguments.push(value);
+	});
 
 	var templateString = compiledTemplate.apply(null, templateArguments);
 
-	if (parameters.componentHash) {
-		templateString = templateString.replace('b-on=', 'b-on-' + parameters.componentHash + '=');
+	if (renderingInstance._kind === 'component') {
+		templateString = templateString.replace(
+			new RegExp(eventHandlersAttributeName + '=', 'g'),
+			eventHandlersAttributeName + '-' + renderingInstance._hash + '='
+		);
+
 		var parentElement = new DOMParser().parseFromString(templateString, 'text/html').querySelector('body *');
 
 		if (parentElement) {
-			parentElement.setAttribute(selectorAttributeName, parameters.componentHash);
+			parentElement.setAttribute(selectorAttributeName, renderingInstance._hash);
 			templateString = parentElement.outerHTML;
 		}
 	}
 
+	renderingInstance._setStatus(renderingInstancesStatuses.rendered);
+
 	return {
 		templateString: templateString,
-		templateRuntime: data['_runtime']
+		templateRuntime: runtime
 	};
 }
