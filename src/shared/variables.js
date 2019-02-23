5b6fe8e9804874cf5c15254ec89e950b73e6a470
Vladimír Macháček <8machy@seznam.cz>
2019-02-23 18:27:11 +0100

    Added configure function, added delimiters customization feature, added config object

--- src/shared/variables.js
+++ src/shared/variables.js
@@ -1,5 +1,22 @@
+import {utils} from "./utils";
+import {initTemplateTokenizer} from "../render/compiler/tokenizeTemplate";
+import {initMacros} from "../render/runtime/macros";
+
 export var Brackets = {
-	devMode: false
+	config: {
+		devMode: false,
+		delimiters: ['{{', '}}']
+	},
+	configure: function (configuration) {
+		if (configuration) {
+			this.config = utils.mergeObjects(this.config, configuration);
+		}
+
+		initTemplateTokenizer(this.config);
+		initMacros(this.config);
+
+		return this;
+	}
 };
 
 export var eventHandlersAttributeName = 'b-on';
