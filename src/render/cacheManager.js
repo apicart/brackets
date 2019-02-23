36d1fee72b96e457b4831d2b82a4ace6e89df0ba
Vladimír Macháček <8machy@seznam.cz>
2019-02-23 18:06:54 +0100

    Added cacheManager as a separated layer that works with cache

---
+++ src/render/cacheManager.js
@@ -0,0 +1,59 @@
+export var cacheManager = {
+	cache: {}
+};
+
+
+/**
+ * @param {string} region
+ * @param {string} cacheKey
+ * @returns {*}
+ */
+cacheManager.getCache = function (region, cacheKey) {
+	if ( ! cacheManager.hasCache(region, cacheKey)) {
+		return null;
+	}
+
+	return cacheManager.cache[region][cacheKey];
+};
+
+
+/**
+ * @param {string} region
+ * @param {string} cacheKey
+ * @param {*} cache
+ * @returns {{cache: {}}}
+ */
+cacheManager.setCache = function (region, cacheKey, cache) {
+	if ( ! cacheManager.hasCacheRegion(region)) {
+		cacheManager.cache[region] = {};
+	}
+
+	if ( ! cacheManager.hasCache(region, cacheKey)) {
+		cacheManager.cache[region][cacheKey] = cache;
+	}
+
+	return cacheManager;
+};
+
+
+/**
+ * @param {string} region
+ * @returns {boolean}
+ */
+cacheManager.hasCacheRegion = function (region) {
+	return region in cacheManager.cache;
+};
+
+
+/**
+ * @param {string} region
+ * @param {string} cacheKey
+ * @returns {boolean}
+ */
+cacheManager.hasCache = function (region, cacheKey) {
+	if ( ! cacheManager.hasCacheRegion(region)) {
+		return false;
+	}
+
+	return cacheKey in cacheManager.cache[region];
+};
